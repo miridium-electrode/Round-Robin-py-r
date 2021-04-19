b83e0262714b19d4658d50372c2aa95940913ab6
miridium-electrode <checkerdance@yahoo.com>
2021-04-19 02:30:57 +0000

    add documentation

--- contoh1.py
+++ contoh1.py
@@ -13,8 +13,29 @@ from tabulate import tabulate
 import csv
 
 def rr(p, bt, q):
+	"""
+	a function that print round robin stats,
+	that is process number, burst time, 
+	waiting time, average waiting time
+
+	Parameter
+	---------
+	p: int
+		number that represent how many process are running
+	bt: list
+		list of number that contain burst time of process 1 to p
+	q: int
+		time quantum, a number that tell how long a process can run
+		before switching to another process
+
+	Returns
+	-------
+	table: dict
+		a dictionary containing list of process number, list of
+		burst time, list of waiting time
+	"""
+	
 	wt = fwt(p, bt, q)
-	# tat = ftat(p, bt, wt)
 	twt = 0
 
 	for i in range(p):
@@ -33,6 +54,27 @@ def rr(p, bt, q):
 
 
 def fwt(p, bt, q):
+	"""
+	a function that calculate waiting time, it take p,
+	bt, and q. Then create a copy of bt. Then endlessly
+	loop to calcutate wt until all bt copy is 0
+
+	Parameter
+	---------
+	p: int
+		number that represent how many process are running
+	bt: list
+		list of number that contain burst time of process 1 to p
+	q: int
+		time quantum, a number that tell how long a process can run
+		before switching to another process
+	
+	Returns
+	-------
+	wt: list
+		waiting time of each process, hence wt
+	"""
+
 	wt = [0] * p
 	rbt = [0] * p
 	t = 0
@@ -59,6 +101,25 @@ def fwt(p, bt, q):
 
 
 def ftat(p, bt, wt):
+	"""
+	function that calculate turnaound time,
+	that is (burst time) + (waiting time)
+
+	Parameter
+	---------
+	p: int
+		number that represent how many process are running
+	bt: list
+		list of number that contain burst time of process 1 to p
+	wt: list
+		waiting time of each process, hence wt
+
+	Returns
+	-------
+	tat: list
+		turnaround time for each process, hence tat 
+	"""
+
 	tat = [0] * p
 	for i in range(p):
 		tat[i] = bt[i] + wt[i]
@@ -66,6 +127,31 @@ def ftat(p, bt, wt):
 
 
 def create_csv_array(table):
+	"""
+	function that take a dictionary and
+	turn it into 2 dimentional array that
+	can be turned into csv file
+	
+	Parameter
+	---------
+	table: dict
+		the dictionary format must be:
+		{
+			"table header 1": [data1, data2, data3, ...],
+			"table header 2": [data1, data2, data3, ...],
+			...
+		}
+
+	Returns
+	-------
+	csv_header: list
+		list of string that will be the csv
+		header file
+	csv_ex: list
+		2 dimentional list(list that contain list) that
+		contain csv data
+	"""
+
 	csv_ex = [[] for i in range(len(table))]
 	csv_header = []
 	for i in range(len(table)):
@@ -79,6 +165,20 @@ def create_csv_array(table):
 
 
 def export_data(header, csv_ex):
+	"""
+	a function that turn list of header
+	and list of list into csv file named
+	round_robin.csv in the same directory
+	that this script reside
+
+	Parameter
+	---------
+	header: list
+		list of string that will be the csv
+		header file
+	csv_ex: list
+		list of list that contain csv data 
+	"""
 	with open("round_robin.csv", "w") as f:
 		f = csv.writer(f, delimiter=",")
 		f.writerow(header)
